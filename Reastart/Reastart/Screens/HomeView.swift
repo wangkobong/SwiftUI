a18e76927b312dc1fccb6a7bf89c7ac194386925
wangkobong <wangkobong@gmail.com>
2022-04-02 15:44:26 +0900

    reusable view를 이용해 HomeView 추가

--- Reastart/Reastart/Screens/HomeView.swift
+++ Reastart/Reastart/Screens/HomeView.swift
@@ -15,14 +15,45 @@ struct HomeView: View {
     // MARK: - BODY
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            
+            Spacer()
+            
+            ZStack {
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+                
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+            }
+            
+            // MARK: - CENTER
+            
+            Text("The Time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            } //: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         } //: VSTACK
     }
 }
---
+++ Reastart/Reastart/Views/CircleGroupView.swift
@@ -0,0 +1,40 @@
+//
+//  CircleGroupView.swift
+//  Reastart
+//
+//  Created by sungyeon kim on 2022/04/02.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    // MARK: - PROPERY
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    // MARK: - BODY
+    
+    
+    var body: some View {
+        ZStack{
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
