3f5f31456a6c652f32848b873f71490fe366bddd
wangkobong <wangkobong@gmail.com>
2022-04-06 18:49:37 +0900

    Pinch Project 생성 및 기본설정

---
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by sungyeon kim on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+        .preferredColorScheme(.dark)
+    }
+}
---
+++ Pinch/Pinch/View/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by sungyeon kim on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
