f9101f2def43754bb03b151410060ef5ded03b87
wangkobong <wangkobong@gmail.com>
2022-04-07 20:46:37 +0900

    DoubleTapGesture로 이미지 확대하기

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+  // MARK: - PROPERTY
+  
+  @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
+  
+  // MARK: - FUNCTION
+  
+  // MARK: - CONTENT
     var body: some View {
-        Text("Hello, world!")
+      NavigationView {
+        ZStack {
+          // MARK: - PAGE IMAGE
+          Image("magazine-front-cover")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(10)
             .padding()
+            .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+            .opacity(isAnimating ? 1 : 0)
+            .scaleEffect(imageScale)
+          // MARK: - 1. TAP GESTURE
+            .onTapGesture(count: 2, perform: {
+              if imageScale == 1 {
+                withAnimation(.spring()) {
+                  imageScale = 5
+                }
+              } else {
+                withAnimation(.spring()) {
+                  imageScale = 1
+                }
+              }
+            })
+        } //: ZSATCK
+        .navigationTitle("Pinch & Zoom")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: {
+          withAnimation(.linear(duration: 1)) {
+            isAnimating = true
+          }
+        })
+      } //: NAVIGATION
+      .navigationViewStyle(.stack)
     }
 }
 
