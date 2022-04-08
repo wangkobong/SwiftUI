8addf662adb6399dde0c1527f333ea1d86f1fe13
wangkobong <wangkobong@gmail.com>
2022-04-08 21:55:01 +0900

    DragGesture 추가

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
   
   @State private var isAnimating: Bool = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
   
   // MARK: - FUNCTION
+  
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
   
   // MARK: - CONTENT
     var body: some View {
@@ -27,6 +35,7 @@ struct ContentView: View {
             .padding()
             .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
             .opacity(isAnimating ? 1 : 0)
+            .offset(x: imageOffset.width, y: imageOffset.height)
             .scaleEffect(imageScale)
           // MARK: - 1. TAP GESTURE
             .onTapGesture(count: 2, perform: {
@@ -35,11 +44,24 @@ struct ContentView: View {
                   imageScale = 5
                 }
               } else {
-                withAnimation(.spring()) {
-                  imageScale = 1
-                }
+                resetImageState()
               }
             })
+          // MARK: - 2. DRAG GESTURE
+            .gesture(
+              DragGesture()
+                .onChanged { value in
+                  print(value)
+                  withAnimation(.linear(duration: 1)) {
+                    imageOffset = value.translation
+                  }
+                }
+                .onEnded { _ in
+                  if imageScale <= 1 {
+                    resetImageState()
+                  }
+                }
+            )
         } //: ZSATCK
         .navigationTitle("Pinch & Zoom")
         .navigationBarTitleDisplayMode(.inline)
