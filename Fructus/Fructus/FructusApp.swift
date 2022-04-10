c95e882ebff45973235f607dd31af3113fdba3fa
wangkobong <wangkobong@gmail.com>
2022-04-10 23:39:42 +0900

    Fructus Project 초기 세팅

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by sungyeon kim on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
