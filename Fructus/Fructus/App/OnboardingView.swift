916ca75c8d37ce6194bf164135809465fb01814e
wangkobong <wangkobong@gmail.com>
2022-04-11 14:45:32 +0900

    카드뷰를 이용한 온보딩뷰 추가

---
+++ Fructus/Fructus/App/OnboardingView.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by sungyeon kim on 2022/04/11.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+  
+  // MARK: - PROPERTIES
+  
+  // MARK: - BODY
+  
+    var body: some View {
+      TabView{
+        ForEach(0..<5) { item in
+          FruitCardView()
+        } //: LOOP
+      } //: TAB
+      .tabViewStyle(PageTabViewStyle())
+      .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
