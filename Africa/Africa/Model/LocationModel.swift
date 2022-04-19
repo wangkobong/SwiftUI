4dcb0dce929e322d4e2c606bc71260a6bfae941e
wangkobong <wangkobong@gmail.com>
2022-04-19 21:44:02 +0900

    MapAnnotation을 활용하여 MapView에 국립공원 위치찍기

---
+++ Africa/Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by sungyeon kim on 2022/04/19.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+  var id: String
+  var name: String
+  var image: String
+  var latitude: Double
+  var longitude: Double
+  
+  // Computed Property
+  var location: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+  }
+}
