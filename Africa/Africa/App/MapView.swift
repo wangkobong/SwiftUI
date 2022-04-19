defe3286bd2318ea20ded2226a460c699473286f
wangkobong <wangkobong@gmail.com>
2022-04-19 22:09:00 +0900

    Animation을 이용해 Custom Annotation Mapview에 구현

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -35,14 +35,64 @@ struct MapView: View {
 //        MapMarker(coordinate: item.location, tint: .accentColor)
         
         // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+//        MapAnnotation(coordinate: item.location) {
+//          Image("logo")
+//            .resizable()
+//            .scaledToFit()
+//            .frame(width: 32, height: 32, alignment: .center)
+//        } //: ANNOTATION
+        
+        // (D) CUSTOM ADVANCED ANNOTATION (it could be interactive)
         MapAnnotation(coordinate: item.location) {
-          Image("logo")
+          MapAnnotationView(location: item)
+        }
+      }) //: MAP
+      .overlay (
+        HStack(alignment: .center, spacing: 12) {
+          Image("compass")
             .resizable()
             .scaledToFit()
-            .frame(width: 32, height: 32, alignment: .center)
-        } //: ANNOTATION
-
-      })
+          .frame(width: 48, height: 48, alignment: .center)
+          
+          VStack(alignment: .leading, spacing: 3) {
+            HStack {
+              Text("Latitude:")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.accentColor)
+              
+              Spacer()
+              
+              Text("\(region.center.latitude)")
+                .font(.footnote)
+                .foregroundColor(.white)
+            } //: HSTACK
+            Divider()
+            
+            HStack {
+              Text("Longitude:")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.accentColor)
+              
+              Spacer()
+              
+              Text("\(region.center.longitude)")
+                .font(.footnote)
+                .foregroundColor(.white)
+            } //: HSTACK
+          }
+        } //: HSTACK
+          .padding(.vertical, 12)
+          .padding(.horizontal, 16)
+          .background(
+            Color.black
+              .cornerRadius(8)
+              .opacity(0.6)
+          )
+          .padding()
+        , alignment: .top
+      )
     }
 }
 
