9d6cde9dac52d27f778e45cfe8b17c8c16514ae2
wangkobong <wangkobong@gmail.com>
2022-08-05 23:49:14 +0900

    Message table view init

---
+++ GroupedMessages/GroupedMessages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupedMessages
+//
+//  Created by sungyeon kim on 2022/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
