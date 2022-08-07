3c083b5c5b63fcb05b031f38c85ec8aed0fe50e9
wangkobong <wangkobong@gmail.com>
2022-08-07 12:06:02 +0900

    섹션헤더에 채팅 날짜 추가

--- GroupedMessages/GroupedMessages/ViewController.swift
+++ GroupedMessages/GroupedMessages/ViewController.swift
@@ -10,26 +10,43 @@ import UIKit
 struct ChatMessage {
     let text: String
     let isIncoming: Bool
+    let date: Date
+}
+
+extension Date {
+    static func dateFromCustomString(customString: String) -> Date {
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter.date(from: customString)!
+    }
 }
 
 class ViewController: UITableViewController {
     
     private let cellID = "id"
     
-    let chatMessages = [
-        ChatMessage(text: "Here's my very first message.", isIncoming: true),
-        ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true),
-        ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap", isIncoming: false),
-        ChatMessage(text: "ㅋㅋㅋ", isIncoming: false),
-    ]
-    
-//    let textMessages = [
-//        "Here's my very first message.",
-//        "I'm going to message another long message that will word wrap",
-//        "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap ",
-//        "Yo, dawg, whaddup!",
-//        "z"
+//    let chatMessages = [
+//        ChatMessage(text: "Here's my very first message.", isIncoming: true),
+//        ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true),
+//        ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap", isIncoming: false),
+//        ChatMessage(text: "ㅋㅋㅋ", isIncoming: false),
 //    ]
+    
+    let chatMessages = [
+        [
+            ChatMessage(text: "Here's my very first message.", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018")),
+            ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018"))
+        ],
+        [
+            ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap", isIncoming: false, date: Date.dateFromCustomString(customString: "09/15/2018")),
+            ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true, date: Date()),
+            ChatMessage(text: "ㅋㅋㅋ", isIncoming: false, date: Date.dateFromCustomString(customString: "09/15/2018"))
+        ],
+        [
+            ChatMessage(text: "Third Section message", isIncoming: true, date: Date.dateFromCustomString(customString: "10/31/2018"))
+        ],
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,16 +56,27 @@ class ViewController: UITableViewController {
         tableView.register(ChatMessageCell.self, forCellReuseIdentifier: ChatMessageCell.identifier)
         tableView.separatorStyle = .none
         tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
-        
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return chatMessages.count
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let firstMessageInSection = chatMessages[section].first else { return "" }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let dateString = dateFormatter.string(from: firstMessageInSection.date)
+        return "Date: \(dateString)"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatMessages[section].count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let message = chatMessages[indexPath.row].text
-        let chatMessage = chatMessages[indexPath.row]
+        let message = chatMessages[indexPath.section][indexPath.row].text
+        let chatMessage = chatMessages[indexPath.section][indexPath.row]
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatMessageCell.identifier, for: indexPath) as? ChatMessageCell else {
             return UITableViewCell()
         }
