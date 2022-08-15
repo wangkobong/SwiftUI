c044bccd754b702906468d3c56f5edd998965fa8
wangkobong <wangkobong@gmail.com>
2022-08-15 23:20:33 +0900

    NavigationLink를 이용해 ItemDetailView 추가

---
+++ iDine/iDine/ItemDetail.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by sungyeon kim on 2022/08/15.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetail()
+    }
+}
