1e1b33b063e35bb386715fb8f61c8acff15e17c4
wangkobong <wangkobong@gmail.com>
2022-08-20 12:32:51 +0900

    first commit

---
+++ AuthIO/AuthIO/AuthIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthIOApp.swift
+//  AuthIO
+//
+//  Created by sungyeon kim on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct AuthIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
