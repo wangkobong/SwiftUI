98a96f89078c8f44dbeee43d8497738165d55abd
wangkobong <wangkobong@gmail.com>
2022-09-15 00:39:59 +0900

    kxWeather 프로젝트 mainView 레이아웃 잡기

--- KxWeather/KxWeather/View/MainView.swift
+++ KxWeather/KxWeather/View/MainView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            GeometryReader { reader in
+                Image("bkg")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: reader.size.width)
+                    .ignoresSafeArea()
+                    .overlay {
+                        Color.black.opacity(0.5)
+                            .ignoresSafeArea()
+                    }
+            }
+            
+            VStack {
+                Text("강남역")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                
+                GeometryReader { reader in
+                    ScrollView(showsIndicators: false) {
+                        VStack {
+                            Color.red
+                            
+                            Color.yellow
+                                .frame(height: 200)
+                            
+                            
+                        }//: VSTACK
+                        .frame(height: reader.size.height)
+                        
+                        Spacer(minLength: reader.safeAreaInsets.bottom)
+                            
+                        
+                        Color.green
+                            .frame(height: 1000)
+                    }
+                }//: GeometryReader
+            }//: VSTACK
+        }//: ZSTACK
     }
 }
 
