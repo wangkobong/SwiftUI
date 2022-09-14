f23c0d2a0c61f71ff52ed4fd389bb89e244fabd4
wangkobong <wangkobong@gmail.com>
2022-09-14 23:01:36 +0900

    kxweather 프로젝트 추가

---
+++ KxWeather/KxWeather/KxWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  KxWeatherApp.swift
+//  KxWeather
+//
+//  Created by sungyeon kim on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct KxWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
