d3f9496bfd4cd5876cf685e47e5203d83fba3dcb
wangkobong <wangkobong@gmail.com>
2022-10-21 01:01:03 +0900

    비동기 async await2

--- swiftUIConcurrency/swiftUIConcurrency/DownloadImageAsync.swift
+++ swiftUIConcurrency/swiftUIConcurrency/DownloadImageAsync.swift
@@ -6,24 +6,50 @@
 //
 
 import SwiftUI
+import Combine
 
 class DownloadImageAsyncImageLoader {
     
     let url = URL(string: "https://picsum.photos/200")!
     
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+                return nil
+            }
+        return image
+    }
+    
     func downloadWithEscaping(completion: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let data = data,
-                let image = UIImage(data: data),
-                let response = response as? HTTPURLResponse,
-                response.statusCode >= 200 && response.statusCode < 300 else {
-                    completion(nil, error)
-                    return
-                }
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
             completion(image, nil)
         }
         .resume()
+    }
+    
+    func downloadWithCombine() -> AnyPublisher<UIImage?, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(handleResponse)
+            .mapError({ $0 })
+            .eraseToAnyPublisher()
+    }
+    
+    /*
+        1. [weak self] 키워드 사용 안해도됨
+        2. 컴플리션 호출 실수 방지. async에서는 리턴을 안한다면 컴파일조차 안됨 -> 더 안전한 코드 작성
+        3.
+     */
+    func downloadWithAsync() async throws -> UIImage? {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
+            return handleResponse(data: data, response: response)
+        } catch  {
+            throw error
+        }
     }
 }
 
@@ -32,12 +58,30 @@ class DownloadImageAsyncViewModel: ObservableObject {
     @Published var image: UIImage? = nil
     
     let loader = DownloadImageAsyncImageLoader()
+    var cancellables = Set<AnyCancellable>()
     
-    func fetchImage() {
-        loader.downloadWithEscaping { [weak self] image, error in
-            if let image = image {
-                self?.image = image
-            }
+    func fetchImage() async {
+        // 이스케이핑 클로저
+//        loader.downloadWithEscaping { [weak self] image, error in
+//            DispatchQueue.main.async {
+//                self?.image = image
+//            }
+//
+//        }
+        
+        //컴바인
+//        loader.downloadWithCombine()
+//            .receive(on: DispatchQueue.main)
+//            .sink { _ in
+//
+//            } receiveValue: { [weak self] image in
+//                self?.image = image
+//            }
+//            .store(in: &cancellables)
+        
+        let image = try? await loader.downloadWithAsync()
+        await MainActor.run {
+            self.image = image
         }
     }
 }
@@ -56,7 +100,9 @@ struct DownloadImageAsync: View {
             }
         }
         .onAppear {
-            viewModel.fetchImage()
+            Task {
+                await viewModel.fetchImage()
+            }
         }
     }
 }
