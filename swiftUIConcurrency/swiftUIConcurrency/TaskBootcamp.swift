de3ed8ec6c5be47977327729a31870a8121b20b6
wangkobong <wangkobong@gmail.com>
2022-10-25 00:25:42 +0900

    Task 학습3

--- swiftUIConcurrency/swiftUIConcurrency/TaskBootcamp.swift
+++ swiftUIConcurrency/swiftUIConcurrency/TaskBootcamp.swift
@@ -14,11 +14,22 @@ class TaskBootcampViewModel: ObservableObject {
 
     
     func fetchImage() async {
+        try? await Task.sleep(nanoseconds: 5_000_000_000)
+        
+//        for x in array {
+//            // work
+//            try? Task.checkCancellation()
+//        }
+        
         do {
             guard let url = URL(string: "https://picsum.photos/200") else { return }
             let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
-            self.image = UIImage(data: data)
-        
+            
+            await MainActor.run(body: {
+                self.image = UIImage(data: data)
+                print("IMAGE RETURNED")
+            })
+            
         } catch  {
             print(error.localizedDescription)
         }
@@ -28,10 +39,24 @@ class TaskBootcampViewModel: ObservableObject {
         do {
             guard let url = URL(string: "https://picsum.photos/200") else { return }
             let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
-            self.image2 = UIImage(data: data)
-        
+            await MainActor.run(body: {
+                self.image2 = UIImage(data: data)
+            })
+
         } catch  {
             print(error.localizedDescription)
+        }
+    }
+}
+
+struct TaskBootcampHomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                NavigationLink("Click me") {
+                    TaskBootcamp()
+                }
+            }
         }
     }
 }
@@ -39,7 +64,7 @@ class TaskBootcampViewModel: ObservableObject {
 struct TaskBootcamp: View {
     
     @StateObject private var viewModel = TaskBootcampViewModel()
-    
+    @State private var fetchImageTask: Task<(), Never>? = nil
     var body: some View {
         VStack(spacing: 40) {
             if let image = viewModel.image {
@@ -56,42 +81,60 @@ struct TaskBootcamp: View {
                     .frame(width: 200, height: 200)
             }
         }
-        .onAppear {
-            
-//            Task {
-//                print(Thread.current)
-//                print(Task.currentPriority)
+        .task {
+            await viewModel.fetchImage()
+        }
+//        .onDisappear {
+//            self.fetchImageTask?.cancel()
+//        }
+//        .onAppear {
+//
+//            self.fetchImageTask = Task {
 //                await viewModel.fetchImage()
 //            }
-//            Task {
-//                print(Thread.current)
-//                print(Task.currentPriority)
-//                await viewModel.fetchImage2()
-//            }
-            Task(priority: .low) {
-                print("LOW: \(Thread.current): \(Task.currentPriority)")
-            }
-            
-            Task(priority: .medium) {
-                print("medium: \(Thread.current): \(Task.currentPriority)")
-            }
-            
-            Task(priority: .high) {
-                print("high: \(Thread.current): \(Task.currentPriority)")
-            }
-            
-            Task(priority: .background) {
-                print("background: \(Thread.current): \(Task.currentPriority)")
-            }
-            
-            Task(priority: .utility) {
-                print("utility: \(Thread.current): \(Task.currentPriority)")
-            }
-            
-            Task(priority: .userInitiated) {
-                print("userInitiated: \(Thread.current): \(Task.currentPriority)")
-            }
-        }
+////            Task {
+////                print(Thread.current)
+////                print(Task.currentPriority)
+////                await viewModel.fetchImage2()
+////            }
+//
+////            Task(priority: .high) {
+//////                try? await Task.sleep(nanoseconds: 2_000_000_000)
+////                await Task.yield()
+////                print("high: \(Thread.current): \(Task.currentPriority)")
+////            }
+//
+////            Task(priority: .userInitiated) {
+////                print("userInitiated: \(Thread.current): \(Task.currentPriority)")
+////            }
+////
+////            Task(priority: .medium) {
+////                print("medium: \(Thread.current): \(Task.currentPriority)")
+////            }
+////
+////            Task(priority: .low) {
+////                print("LOW: \(Thread.current): \(Task.currentPriority)")
+////            }
+////
+////            Task(priority: .utility) {
+////                print("utility: \(Thread.current): \(Task.currentPriority)")
+////            }
+////
+////            Task(priority: .background) {
+////                print("background: \(Thread.current): \(Task.currentPriority)")
+////            }
+////
+//
+////            Task(priority: .low) {
+////                print("low: \(Thread.current): \(Task.currentPriority)")
+////
+////                Task.detached {
+////                    print("detached: \(Thread.current): \(Task.currentPriority)")
+////                }
+////            }
+//
+//
+//        }
     }
 }
 
