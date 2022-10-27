54f1d22a78f6ebd0b987cda00710bcc9b8e631bb
wangkobong <wangkobong@gmail.com>
2022-10-27 23:39:36 +0900

    continuation 학습

---
+++ swiftUIConcurrency/swiftUIConcurrency/CheckedContinuationBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  CheckedContinuationBootcamp.swift
+//  swiftUIConcurrency
+//
+//  Created by sungyeon kim on 2022/10/27.
+//
+
+import SwiftUI
+
+struct CheckedContinuationBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckedContinuationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckedContinuationBootcamp()
+    }
+}
