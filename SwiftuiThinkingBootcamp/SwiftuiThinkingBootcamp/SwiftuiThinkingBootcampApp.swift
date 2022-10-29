ecd0be6c486a0199fc519471ad8cb7d4c43ea772
wangkobong <wangkobong@gmail.com>
2022-10-29 21:54:27 +0900

    SwiftuiThinkingBootcamp #1 TEXT

---
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/SwiftuiThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiThinkingBootcampApp.swift
+//  SwiftuiThinkingBootcamp
+//
+//  Created by sungyeon kim on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
