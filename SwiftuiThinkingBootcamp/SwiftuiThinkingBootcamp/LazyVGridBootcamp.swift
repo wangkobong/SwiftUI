35a216c25ec8c1df64d34b7dcb95117782da85d5
wangkobong <wangkobong@gmail.com>
2022-11-11 00:04:12 +0900

    SwiftuiThinkingBootcamp SafeArea

--- SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/LazyVGridBootcamp.swift
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/LazyVGridBootcamp.swift
@@ -10,17 +10,61 @@ import SwiftUI
 struct LazyVGridBootcamp: View {
     
     let columns: [GridItem] = [
-        GridItem(.flexible(), spacing: nil, alignment: nil),
-    
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
     ]
     
     var body: some View {
-        LazyVGrid(columns: columns) {
-            ForEach(0..<50) { index in
-                Rectangle()
-                    .frame(height: 50)
-            }
+        ScrollView {
             
+            Rectangle()
+                .fill(.orange)
+                .frame(height: 400)
+            
+            LazyVGrid(
+                columns: columns,
+                alignment: .center,
+                spacing: 6,
+                pinnedViews: [.sectionHeaders]) {
+                    Section {
+                        ForEach(0..<20) { index in
+                            Rectangle()
+                                .frame(height: 150)
+                        }
+                    } header: {
+                        Text("Section 1")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(.blue)
+                            .font(.title)
+                            
+                    }
+                    
+                    Section {
+                        ForEach(0..<20) { index in
+                            Rectangle()
+                                .fill(.green)
+                                .frame(height: 150)
+                        }
+                    } header: {
+                        Text("Section 2")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(.green)
+                            .font(.title)
+                            
+                    }
+
+
+                }
+            
+//            LazyVGrid(columns: columns) {
+//                ForEach(0..<50) { index in
+//                    Rectangle()
+//                        .frame(height: 50)
+//                }
+//            }
         }
     }
 }
---
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/SafeAreaBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  SafeAreaBootcamp.swift
+//  SwiftuiThinkingBootcamp
+//
+//  Created by sungyeon kim on 2022/11/11.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SafeAreaBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SafeAreaBootcamp()
+    }
+}
