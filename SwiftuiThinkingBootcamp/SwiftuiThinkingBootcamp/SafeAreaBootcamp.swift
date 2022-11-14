5a3c9f699305277fe8ca52e9d0a4be313ecb6215
wangkobong <wangkobong@gmail.com>
2022-11-14 23:51:10 +0900

    SwiftuiThinkingBootcamp Button

--- SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/SafeAreaBootcamp.swift
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/SafeAreaBootcamp.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct SafeAreaBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+          
+            
+            ScrollView {
+                VStack {
+                    Text("Title goes here")
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    ForEach(0..<10) { index in
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(.white)
+                            .frame(height: 150)
+                            .shadow(radius: 10)
+                            .padding(20)
+                    }
+                }
+            }
+//            .background(.blue)
+            .background(
+                Color.red
+                    .edgesIgnoringSafeArea(.all)
+            )
+            
+        }
+       
+//        ZStack {
+//            // background
+//            Color.blue
+//                .edgesIgnoringSafeArea(.all)
+//
+//            // foreground
+//            VStack {
+//                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//                Spacer()
+//            }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(.red)
+//        }
     }
 }
 
