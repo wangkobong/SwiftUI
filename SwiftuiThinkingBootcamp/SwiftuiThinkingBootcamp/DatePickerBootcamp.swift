8d805dbe16b73d48a79b4d7e41912f706858627e
wangkobong <wangkobong@gmail.com>
2022-12-06 23:48:55 +0900

    SwiftuiThinkingBootcamp DatePicker

---
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/DatePickerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  DatePickerBootcamp.swift
+//  SwiftuiThinkingBootcamp
+//
+//  Created by sungyeon kim on 2022/12/06.
+//
+
+import SwiftUI
+
+struct DatePickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DatePickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickerBootcamp()
+    }
+}
