ff1a4eb8a5c34e91186a5caa7530ca7174d13331
wangkobong <wangkobong@gmail.com>
2022-12-13 23:47:39 +0900

    PageTabViewStyleBootcamp

---
+++ CombineWithMVVM/CombineWithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineWithMVVM
+//
+//  Created by sungyeon kim on 2022/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/PageTabViewStyleBootcamp.swift
@@ -0,0 +1,22 @@
+//
+//  PageTabViewStyleBootcamp.swift
+//  SwiftuiThinkingBootcamp
+//
+//  Created by sungyeon kim on 2022/12/13.
+//
+
+import SwiftUI
+
+struct PageTabViewStyleBootcamp: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+struct PageTabViewStyleBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PageTabViewStyleBootcamp()
+    }
+}
