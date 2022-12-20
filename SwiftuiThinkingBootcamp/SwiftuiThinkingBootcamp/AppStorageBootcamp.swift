69cd5fad2e51b410bf74b671aba0c4b3fd619e85
wangkobong <wangkobong@gmail.com>
2022-12-21 00:27:32 +0900

    SwiftUIThinking AppStorage#1

---
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/AppStorageBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  AppStorageBootcamp.swift
+//  SwiftuiThinkingBootcamp
+//
+//  Created by sungyeon kim on 2022/12/21.
+//
+
+import SwiftUI
+
+struct AppStorageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppStorageBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AppStorageBootcamp()
+    }
+}
