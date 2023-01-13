42104442f934b30f5b60664bc892ab8cb7a0a479
wangkobong <wangkobong@gmail.com>
2023-01-13 23:45:59 +0900

    SwiftUIThinking WeakSelfBootcamp

---
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/WeakSelfBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  WeakSelfBootcamp.swift
+//  SwiftuiThinkingBootcamp
+//
+//  Created by sungyeon kim on 2023/01/13.
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeakSelfBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakSelfBootcamp()
+    }
+}
