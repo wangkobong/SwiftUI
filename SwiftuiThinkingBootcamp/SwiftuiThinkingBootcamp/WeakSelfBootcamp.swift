383b24d860891d3971b80bfd43640e08c621d1cf
wangkobong <wangkobong@gmail.com>
2023-01-14 19:05:52 +0900

    SwiftuiThinking WeakselfBootcamp #2

--- SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/WeakSelfBootcamp.swift
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/WeakSelfBootcamp.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct WeakSelfBootcamp: View {
+    
+    @AppStorage("count") var count: Int?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            NavigationLink("Navigate", destination: WeakSelfSecondScreen())
+                .navigationTitle("Screen 1")
+        }
+        .overlay (
+                Text("\(count ?? 0)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.green.cornerRadius(10))
+                , alignment: .topTrailing
+        )
+        
+    }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var viewModel = WeakSelfSecondScreenViewModel()
+    
+    
+    var body: some View {
+        Text("Second View")
+            .font(.largeTitle)
+            .foregroundColor(.red)
+        
+        if let data = viewModel.data {
+            Text(data)
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+
+    @Published var data: String? = nil
+    
+    init() {
+        print("INIT NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("DEINIT!!!")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData() {
+        data = "NEW DATA!!"
     }
 }
 
