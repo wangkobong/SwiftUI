d9c0cdb4e8696d6bee80e6d7ac4e4b8d486fffe6
wangkobong <wangkobong@gmail.com>
2023-01-21 23:54:08 +0900

    SwiftuiThinking filemanager #2

--- SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/Continued Learning/FileManagerBootcamp.swift
+++ SwiftuiThinkingBootcamp/SwiftuiThinkingBootcamp/Continued Learning/FileManagerBootcamp.swift
@@ -13,8 +13,21 @@ class localFileManger {
     
     func saveImage(image: UIImage, name: String) {
         
-        image.jpegData(compressionQuality: 1.0)
-        image.pngData()
+        guard
+            let data = image.jpegData(compressionQuality: 1.0) else {
+            print("Error getting data.")
+            return
+        }
+        
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let directory2 = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        let directory3 = FileManager.default.temporaryDirectory
+        
+        print(directory)
+        print(directory2)
+        print(directory3)
+        
+        
     }
 }
 
@@ -22,12 +35,18 @@ class FileManagerViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
     let imageName: String = "fish"
+    let manager = localFileManger.shared
     init() {
         getImageFromAssetsFolder()
     }
     
     func getImageFromAssetsFolder() {
         image = UIImage(named: imageName)
+    }
+    
+    func saveImage() {
+        guard let image = image else { return }
+        manager.saveImage(image: image, name: imageName)
     }
 }
 
