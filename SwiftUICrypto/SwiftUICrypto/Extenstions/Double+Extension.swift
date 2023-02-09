8ec9d804811c1e8f4e575131bdddaf776f72b599
wangkobong <wangkobong@gmail.com>
2023-02-09 23:45:20 +0900

    SwiftuiCrypto add CoinRowView

---
+++ SwiftUICrypto/SwiftUICrypto/Extenstions/Double+Extension.swift
@@ -0,0 +1,81 @@
+//
+//  Double.swift
+//  SwiftUICrypto
+//
+//  Created by sungyeon kim on 2023/02/09.
+//
+
+import Foundation
+
+extension Double {
+    
+    /// Converts a Double into a Currency with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a String  with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.000"
+    }
+    
+    /// Converts a Double into a Currency with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// Convert 12.3456 to $12.3456
+    /// Convert 0.123456 to $0.123456
+    /// ```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 6
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a String  with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// Convert 12.3456 to "$12.3456"
+    /// Convert 0.123456 to "$0.123456"
+    /// ```
+    func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number) ?? "$0.000"
+    }
+    
+    /// Converts a Double into string representation
+    /// ```
+    /// Convert 1.2345 to "1.23"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// Converts a Double into string representation with percent symbol
+    /// ```
+    /// Convert 1.2345 to "1.23%"
+    /// ```
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
