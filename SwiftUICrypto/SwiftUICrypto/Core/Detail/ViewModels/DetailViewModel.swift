1e5f1a502a7752fa78223008ea5e51364d321dd4
wangkobong <wangkobong@gmail.com>
2023-03-02 21:26:36 +0900

    SwiftuiCrypto downloaded data in grid #2

--- SwiftUICrypto/SwiftUICrypto/Core/Detail/ViewModels/DetailViewModel.swift
+++ SwiftUICrypto/SwiftUICrypto/Core/Detail/ViewModels/DetailViewModel.swift
@@ -10,7 +10,10 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
-    let coin: CoinModel
+    @Published var overviewStatistics: [StatisticModel] = []
+    @Published var additionalStatistics: [StatisticModel] = []
+    
+    @Published var coin: CoinModel
     private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
@@ -22,8 +25,52 @@ class DetailViewModel: ObservableObject {
     
     private func addSubscribers() {
         coinDetailService.$coinDetails
-            .sink { coinDetails in
-                print("coinDetails: \(coinDetails)")
+            .combineLatest($coin)
+            .map({ (coinDetailModel, coinModel) -> (overview: [StatisticModel], additional: [StatisticModel]) in
+                
+                // overview
+                let price = coinModel.currentPrice.asCurrencyWith6Decimals()
+                let pricePercentChange = coinModel.priceChangePercentage24H
+                let priceStat = StatisticModel(title: "currentPrice", value: price, percentageChange: priceChange)
+                let marketCap = "$" + (coinModel.marketCap?.formattedWithAbbreviations() ?? "")
+                let marketCapChange = coinModel.marketCapChangePercentage24H
+                let marketCapStat = StatisticModel(title: "Market Capialization", value: marketCap, percentageChange: marketCapChange)
+                
+                let rank = "\(coinModel.rank)"
+                let rankStat = StatisticModel(title: "Rank", value: rank)
+                
+                let volume = "$" + (coinModel.totalVolume?.formattedWithAbbreviations() ?? "")
+                let volumeStat = StatisticModel(title: "Volume", value: volume)
+                
+                let overViewArray: [StatisticModel] = [
+                    priceStat,
+                    marketCapStat,
+                    rankStat,
+                    volumeStat
+                ]
+                
+                // additional
+                let high = coinModel.high24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let highStat = StatisticModel(title: "24h High", value: high)
+                
+                let low = coinModel.low24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let lowStat = StatisticModel(title: "24h Low", value: low)
+                
+                let priceChange = coinModel.priceChange24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let pricePercentChange2 = coinModel.priceChangePercentage24H
+                let priceChangeStat = StatisticModel(title: "24h Price Change", value: price, percentageChange: pricePercentChange2)
+                
+                let marketCapChange = "$" + (coinModel.marketCapChange24H?.formattedWithAbbreviations() ?? "" )
+                let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+                
+                
+                
+                return (overViewArray, [])
+            })
+            .sink { returnedArrays in
+                print("RECIEVED COIN DETAIL DATA")
+                print(returnedArrays.overview)
+                print(returnedArrays.additional)
             }
             .store(in: &cancellables)
     }
