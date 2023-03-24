d944f3f17dc65c7e75e58272cba53d43a05f4909
wangkobong <wangkobong@gmail.com>
2023-03-24 22:45:19 +0900

    setup Firebase Bootcamp

---
+++ SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiFirebaseBootcampApp.swift
+//  SwiftuiFirebaseBootcamp
+//
+//  Created by sungyeon kim on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
