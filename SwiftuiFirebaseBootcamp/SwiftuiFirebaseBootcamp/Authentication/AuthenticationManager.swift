f3d4c849e3571f21e1963e147d269a4b59b8185b
wangkobong <wangkobong@gmail.com>
2023-04-06 21:22:37 +0900

    SwiftuiFirebaseBootcamp add google signin

--- SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/Authentication/AuthenticationManager.swift
+++ SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/Authentication/AuthenticationManager.swift
@@ -33,6 +33,16 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: user)
     }
     
+    func singOut() throws {
+        try Auth.auth().signOut()
+    }
+    
+
+}
+
+// MARK: - SIGN IN EMAIL
+extension AuthenticationManager {
+    
     @discardableResult
     func createUser(email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
@@ -43,10 +53,6 @@ final class AuthenticationManager {
     func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
         return AuthDataResultModel(user: authDataResult.user)
-    }
-    
-    func singOut() throws {
-        try Auth.auth().signOut()
     }
     
     func resetPassword(email: String) async throws {
@@ -67,5 +73,20 @@ final class AuthenticationManager {
         }
         
         try await user.updateEmail(to: email)
+    }
+}
+
+// MARK: - SIGN IN SSO
+extension AuthenticationManager {
+    
+    @discardableResult
+    func signInWithGoogle(tokens: GoogleSignInResultModel) async throws -> AuthDataResultModel {
+        let credential = GoogleAuthProvider.credential(withIDToken: tokens.idToken, accessToken: tokens.accessToken)
+        return try await signIn(credential: credential)
+    }
+    
+    func signIn(credential: AuthCredential) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(with: credential)
+        return AuthDataResultModel(user: authDataResult.user)
     }
 }
--- SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/Authentication/AuthenticationView.swift
+++ SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/Authentication/AuthenticationView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 import GoogleSignIn
 import GoogleSignInSwift
+import FirebaseAuth
 
+struct GoogleSignInResultModel {
+    let idToken: String
+    let accessToken: String
+}
+
+@MainActor
 final class AuthenticationViewModel: ObservableObject {
     
     func signInGoogle() async throws {
         
-//        let something = GIDSignIn.sharedInstance.signIn(withPresenting: <#T##UIViewController#>)
+        guard let topVC = Utilities.shared.topViewController() else {
+            throw URLError(.cannotFindHost)
+        }
+        
+        let gidSignInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        
+        guard let idToken: String = gidSignInResult.user.idToken?.tokenString else {
+            throw URLError(.badServerResponse)
+        }
+        let accessToken: String = gidSignInResult.user.accessToken.tokenString
+        
+        let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken)
+        try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+        
     }
     
 }
@@ -38,7 +58,15 @@ struct AuthenticationView: View {
             }
             
             GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .dark, style: .standard, state: .normal)) {
-                
+                Task {
+                    do {
+                        try await viewModel.signInGoogle()
+                        print("구글로그인 성공")
+                        showSignInView = false
+                    } catch {
+                        print(error)
+                    }
+                }
             }
             
             Spacer()
