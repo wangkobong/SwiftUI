bb3693e86b8b6d4112060050bf07de9bba000d7f
wangkobong <wangkobong@gmail.com>
2023-04-07 22:42:51 +0900

    SwiftuiFirebaseBootcamp add google signin helper

--- SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/Authentication/AuthenticationView.swift
+++ SwiftuiFirebaseBootcamp/SwiftuiFirebaseBootcamp/Authentication/AuthenticationView.swift
@@ -10,28 +10,13 @@ import GoogleSignIn
 import GoogleSignInSwift
 import FirebaseAuth
 
-struct GoogleSignInResultModel {
-    let idToken: String
-    let accessToken: String
-}
-
 @MainActor
 final class AuthenticationViewModel: ObservableObject {
     
     func signInGoogle() async throws {
         
-        guard let topVC = Utilities.shared.topViewController() else {
-            throw URLError(.cannotFindHost)
-        }
-        
-        let gidSignInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
-        
-        guard let idToken: String = gidSignInResult.user.idToken?.tokenString else {
-            throw URLError(.badServerResponse)
-        }
-        let accessToken: String = gidSignInResult.user.accessToken.tokenString
-        
-        let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken)
+        let helper = SignInGoogleHelper()
+        let tokens = try await helper.signIn()
         try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
         
     }
